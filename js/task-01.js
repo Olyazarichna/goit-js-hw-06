f109d1a197703e2e644147518c1f4bf922a350a6
Olyazarichna <olya.zarichna@gmail.com>
2022-04-19 14:34:16 +0300

    first ex

---
+++ js/task-01.js
@@ -0,0 +1,16 @@
+const countOfList = document.querySelectorAll('ul .item');
+console.log('Number of categories:', countOfList.length);
+
+
+
+const titleEl = document.querySelectorAll('h2');
+
+const numberListEl = document.querySelectorAll('.item ul');
+
+for(let i = 0; i < numberListEl.length; i += 1) {
+    
+}
+for(let i = 0; i < titleEl.length; i += 1) {
+    console.log('Category:', titleEl[i].textContent);
+    console.log('Elements:' ,numberListEl[i].children.length);
+}
