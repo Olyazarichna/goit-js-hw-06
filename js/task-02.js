2d0ce05843137a7ca60e00be8a944658bbc313da
Olyazarichna <olya.zarichna@gmail.com>
2022-04-19 15:34:24 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,23 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const elements = ingredients.map(ingredient => {
+  const listEl = document.createElement('li');
+  listEl.classList.add('item');
+  listEl.textContent = ingredient;
+  return listEl;
+});
+
+// const elements = [];
+// for(let i = 0; i < ingredients.length; i += 1) {
+
+// const listEl = document.createElement('li');
+// listEl.classList.add('item');
+// listEl.textContent = ingredients[i];
+
+// console.log(listEl);
+
+// elements.push(listEl);
+// }
+
+const addEl = document.querySelector('#ingredients');
+addEl.after(...elements);
