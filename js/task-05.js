65802faf12d2a9fcb0c39da96e1f921865028ac6
Olyazarichna <olya.zarichna@gmail.com>
2022-04-22 11:01:43 +0300

    task 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,9 @@
 const inputEl = document.querySelector('#name-input');
-console.log(inputEl);
+
 const outputEl = document.querySelector('#name-output');
-console.log(outputEl);
+
 
 inputEl.addEventListener('input', event => {
-  outputEl.textContent = event.currentTarget.value;
+ outputEl.textContent = event.currentTarget.value;
 });
+
