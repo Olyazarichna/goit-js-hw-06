fe5052c446db9bc4e59b0e6eca99f721dbbe1226
Olyazarichna <olya.zarichna@gmail.com>
2022-04-22 14:25:10 +0300

    task 7

---
+++ js/task-07.js
@@ -0,0 +1,8 @@
+const inputEl = document.querySelector('#font-size-control');
+const spanEl = document.querySelector('#text');
+console.log(inputEl);
+console.log(spanEl);
+
+inputEl.addEventListener('input', event => {
+  spanEl.style.fontSize = event.currentTarget.value + 'px';
+});
