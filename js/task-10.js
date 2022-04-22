1406a2b1d44768c0b8e31c3400039e5eb8409e3a
Olyazarichna <olya.zarichna@gmail.com>
2022-04-22 21:28:03 +0300

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,40 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const divAdd = document.querySelector('#boxes');
+const inputEl = document.querySelector('input[type="number"]');
+const createEl = document.querySelector('button[data-create]');
+const destroyEl = document.querySelector('button[data-destroy]');
+
+let amount = Number('');
+console.log(amount);
+
+createEl.addEventListener('click', onCreate);
+destroyEl.addEventListener('click', destroyBoxes);
+inputEl.addEventListener('input', amountOfBoxes);
+
+function amountOfBoxes(event) {
+  amount = event.currentTarget.value;
+}
+
+function createBoxes(amount) {
+  let boxes = [];
+  let size = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+    boxes.push(
+      `<div style= "width:${size}px; height: ${size}px; background-color: ${getRandomHexColor()}"> </div>`
+    );
+    size += 10;
+  }
+  divAdd.insertAdjacentHTML('afterbegin', boxes.join(''));
+}
+
+function onCreate() {
+  createBoxes(amount);
+}
+
+function destroyBoxes() {
+  divAdd.innerHTML = '';
+}
