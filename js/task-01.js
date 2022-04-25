adecc48f0c31df7b4cd1d2f03baa679536c74221
Olyazarichna <olya.zarichna@gmail.com>
2022-04-25 14:00:35 +0200

    task 1 forEach

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,21 @@
-const countOfList = document.querySelectorAll('ul .item');
-console.log('Number of categories:', countOfList.length);
+const countOfList = document.querySelectorAll('.item');
+console.log('Number of categories: ', countOfList.length);
 
-const titleEl = document.querySelectorAll('h2');
+countOfList.forEach(list => {
+  const titleEl = list.querySelector('h2').textContent;
+  const numberListEl = list.querySelector('ul').children.length;
+  console.log('Category: ', titleEl);
+  console.log('Elements:', numberListEl);
+});
 
-const numberListEl = document.querySelectorAll('.item ul');
+// const countOfList = document.querySelectorAll('ul .item');
+// console.log('Number of categories:', countOfList.length);
+// const titleEl = document.querySelectorAll('h2');
 
-for (let i = 0; i < numberListEl.length; i += 1) {}
-for (let i = 0; i < titleEl.length; i += 1) {
-  console.log('Category:', titleEl[i].textContent);
-  console.log('Elements:', numberListEl[i].children.length);
-}
+// const numberListEl = document.querySelectorAll('.item ul');
+
+// for (let i = 0; i < numberListEl.length; i += 1) {}
+// for (let i = 0; i < titleEl.length; i += 1) {
+//   console.log('Category:', titleEl[i].textContent);
+//   console.log('Elements:', numberListEl[i].children.length);
+// }
