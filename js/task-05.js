01345645a5765a3671fa4ec9618818d17691365b
Olyazarichna <olya.zarichna@gmail.com>
2022-04-25 13:33:14 +0200

    task 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,12 +1,10 @@
 const inputEl = document.querySelector('#name-input');
-
 const outputEl = document.querySelector('#name-output');
 
-
 inputEl.addEventListener('input', event => {
-    if(inputEl.value== ''){
-        outputEl.textContent = "Anonymous";
-    }
- outputEl.textContent = event.target.value;
-
+  if (event.currentTarget.value === '') {
+    outputEl.textContent = 'Anonymous';
+  } else {
+    outputEl.textContent = event.currentTarget.value;
+  }
 });
