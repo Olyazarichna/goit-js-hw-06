b175a011237be9a1e08219e3590444d31684ddc2
Olyazarichna <olya.zarichna@gmail.com>
2022-04-26 20:40:59 +0200

    task 6

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@ const inputEl = document.querySelector('#validation-input');
 console.log(inputEl);
 
 inputEl.addEventListener('blur', () => {
-  if (inputEl.value.length >= inputEl.dataset.length) {
+  if (inputEl.value.length == inputEl.dataset.length) {
     inputEl.classList.remove('invalid');
     inputEl.classList.add('valid');
   } else {
